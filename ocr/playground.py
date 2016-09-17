a5ddb973b3d8bd19be2118e47e2cda482668a7d3
vhartman <vhartman@student.ethz.ch>
2016-09-17 03:14:12 +0200

    Playing around with the JSON file

--- ocr/playground.py
+++ ocr/playground.py
@@ -1,14 +1,15 @@
 import json
 from pprint import pprint
 
-
-
-
-
 if __name__ == '__main__':
 
     with open('data.json') as data_file:
         data = json.load(data_file)
 
-    pprint(data)
+    test = {"Name": 1, "Test": 2}
 
+    for value in data["textAnnotations"]:
+        # description field is the identified word
+        pprint(value["description"])
+        # boundingPoly/vertices gives the coordinates
+        pprint(value["boundingPoly"]["vertices"])
