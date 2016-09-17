957246b04d75f76d8074ebdade03d95695f6bd70
vhartman <vhartman@student.ethz.ch>
2016-09-17 10:32:13 +0200

    Request

--- OcrApiRequest/ocr.py
+++ OcrApiRequest/ocr.py
@@ -8,30 +8,31 @@ from oauth2client.client import GoogleCredentials
 
 DISCOVERY_URL='https://{api}.googleapis.com/$discovery/rest?version={apiVersion}'
 
-def request(photo_file):
+def request(image):
     """Run a label request on a single image"""
 
     credentials = GoogleCredentials.get_application_default()
     service = discovery.build('vision', 'v1', credentials=credentials,
                               discoveryServiceUrl=DISCOVERY_URL)
 
-    with open(photo_file, 'rb') as image:
-        image_content = base64.b64encode(image.read())
-        service_request = service.images().annotate(body={
-            'requests': [{
-                'image': {
-                    'content': image_content.decode('UTF-8')
-                },
-                'features': [{
-                    'type': 'TEXT_DETECTION'
-                }]
+    image_content = base64.b64encode(image.read())
+    service_request = service.images().annotate(body={
+        'requests': [{
+            'image': {
+                'content': image_content.decode('UTF-8')
+            },
+            'features': [{
+                'type': 'TEXT_DETECTION'
             }]
-        })
-        response = service_request.execute()
+        }]
+    })
+    response = service_request.execute()
 
-        #label = response['responses'][0]['labelAnnotations'][0]['description']
-        #print('Found label: %s for %s' % (label, photo_file))
-        return response
+    #label = response['responses'][0]['labelAnnotations'][0]['description']
+    #print('Found label: %s for %s' % (label, photo_file))
+    return response
 
 if __name__ == '__main__':
-    request('../Data/20160916_234152.jpg')
+    with open('../Data/20160916_234152.jpg', 'rb') as image:
+        request(image)
+    #request('../Data/20160916_234152.jpg')
