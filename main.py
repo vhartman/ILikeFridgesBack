5a279dcf669402943a62772cfb3c58d939c64211
vhartman <vhartman@student.ethz.ch>
2016-09-17 20:20:14 +0200

    Deleted old code

--- main.py
+++ main.py
@@ -21,25 +21,17 @@ def remove_lower(resp):
 
     return res
 
-# def remove_lower(resp):
-#     res = []
-#     for attr in response['responses'][0]['textAnnotations']:
-#         #print attr
-#         if attr['description'] == 'TOTAL':
-#             y_lim = attr['boundingPoly']['vertices'][0]['y']
-#             break
-#     for attr in response['responses'][0]['textAnnotations']:
-#         if attr['boundingPoly']['vertices'][0]['y'] < y_lim:
-#             res.append(attr)
-#
-#     return res
+image = cv2.imread('Data/20160917_091338.jpg')
+img_s = transform.resize(image, height = 500)
 
-image = cv2.imread('Data/20160916_234205.jpg')
-
-angle = compute_angle(transform.resize(image, height = 500));
-img_rot = transform.rotate(image, angle/3.1415*180)
+angle = compute_angle(img_s)
+img_rot = transform.rotate(image, angle/3.1415*180 - 90)
 
 warped = transform_receipt_image(image)
+
+cv2.imshow("Rotated", img_rot)
+cv2.imshow("Warped", warped)
+cv2.waitKey(0)
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'Hack Zurich 2016 1022-9213bc019af2.json'
 response = ocr.request(warped)
--- ocr/angle.py
+++ ocr/angle.py
@@ -7,7 +7,7 @@ def compute_angle(image):
     img_gr = cv2.cvtColor(img_d, cv2.COLOR_BGR2GRAY)
     (thresh, img_bw) = cv2.threshold(img_gr, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-    cv2.imshow("BW", img_bw)
+    # cv2.imshow("BW", img_bw)
 
     fft = np.fft.fft2(img_bw);
     ffts = np.fft.fftshift(fft)
@@ -16,7 +16,7 @@ def compute_angle(image):
     maximum = max(mag.max(axis = 1))
     mag_n = mag/maximum
 
-    print maximum
+    # print maximum
 
     w = len(mag_n)
     h = len(mag_n[0])
@@ -36,18 +36,18 @@ def compute_angle(image):
                 max_hor_d = abs(hor_d)
                 angle = np.arctan2(ver_d*h, hor_d*w)
 
-    print angle
+    # print angle
 
-    cv2.imshow("Original", img_d)
-    cv2.waitKey(0)
+    # cv2.imshow("Original", img_d)
+    # cv2.waitKey(0)
 
     #mag_grey = cv2.cvtColor(mag, cv2.COLOR_BGR2GRAY)
     #(_, mag_thresh) = cv2.threshold(mag_grey, 128, 255, cv2.THRESH_BINARY)
 
-    plt.subplot(121),plt.imshow(img_bw, cmap = 'gray')
-    plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122),plt.imshow(mag)
-    plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
-    plt.show()
+    # plt.subplot(121),plt.imshow(img_bw, cmap = 'gray')
+    # plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+    # plt.subplot(122),plt.imshow(mag)
+    # plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+    # plt.show()
 
     return angle
